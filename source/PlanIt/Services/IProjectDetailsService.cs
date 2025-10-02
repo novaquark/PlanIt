bec76d8e5e5ee51735fb575882d09ef297358e7f
Krutika Patel <krpatel@totam.biz>
2025-10-02 14:05:33 +0530

    Changed Top bar of the app and added tasks summary

--- source/PlanIt/Services/IProjectDetailsService.cs
+++ source/PlanIt/Services/IProjectDetailsService.cs
@@ -3,10 +3,12 @@
     public interface IProjectDetailsService
     {
         string NextMilestoneName { get; set; }
+        string CurrentSprint { get; set; }
     }
 
     public class ProjectDetailsService : IProjectDetailsService
     {
         public string NextMilestoneName { get; set; } = "None";
+        public string CurrentSprint { get; set; } = "S30";
     }
 }
